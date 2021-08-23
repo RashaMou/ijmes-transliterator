75d6f60548830c10587a291a4e06dc8279adb73f
Rasha Moumneh <rasha@fastmailteam.com>
2021-08-23 19:18:28 -0400

    Basic svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import axios from "axios";
+  export let text;
+  let result;
+
+  async function handleSubmit() {
+    const res = await axios.post("http://localhost:5001/transliterate", {
+      text,
+    });
+    result = res.data;
+    text = "";
+  }
+</script>
+
+<main>
+  <h1>IJMES Transliterator</h1>
+  <textarea bind:value={text} rows="3" />
+  <button on:click|preventDefault={handleSubmit}>Transliterate</button>
+  <div>
+    {#if result}
+      <p>{result}</p>
+    {/if}
+  </div>
+</main>
+
+<style>
+  textarea {
+    display: block;
+  }
+</style>
